59e0347bea5b5e7b932410d5c9a2e098616b13b8
skkhinchi <khinchi.skumar@gmail.com>
2026-04-14 17:01:18 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Footer } from './components/layout/Footer'
 import { Nav } from './components/layout/Nav'
 import { PageBackdrop } from './components/layout/PageBackdrop'
@@ -26,13 +26,13 @@ function HomePage() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <PageBackdrop />
       <Nav />
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
